9a95461b60aa0247c03e3f233c7d72f378d66d83
Haruna Musoke (WIN) <musokeharuna90@gmail.com>
2025-10-19 14:58:24 +0300

    Match platform

--- src/access/adminOnly.ts
+++
@@ -1,3 +0,0 @@
-import { Access } from 'payload'
-
-export const adminOnly: Access = ({ req }) => req.user?.role === 'admin'
--- src/common/components/SubmitSaveButton.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { SaveButton } from '@payloadcms/ui'
-import type { SaveButtonServerProps } from 'payload'
-
-export default function SubmitSaveButton({}: SaveButtonServerProps) {
-  return <SaveButton label="Submit" />
-}
